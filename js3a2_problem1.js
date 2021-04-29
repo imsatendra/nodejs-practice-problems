1d85a912244a3cb3195690012ca49b4b5755e44c
satendra <satendrakr.7@gmail.com>
2021-04-30 00:04:58 +0530

    add solution of sum of all numbers in an array

---
+++ js3a2_problem1.js
@@ -0,0 +1,11 @@
+const numbers = [1,2,3,4];
+
+function sumOfNumber(numbers) {
+    let sum = 0;
+    numbers.forEach(number => {
+        sum = sum + number;
+    })
+    return sum;
+}
+
+console.log(sumOfNumber(numbers));
